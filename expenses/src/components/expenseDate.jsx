0693069f243e969a7d60822fcf820ab178efa51d
Prait Kirilo <prait.kirilo@gmail.com>
2026-01-09 11:32:17 +0200

    Ülesanne 1 - kujunduse lisamine komponendile

--- expenses/src/components/expenseDate.jsx
+++ expenses/src/components/expenseDate.jsx
@@ -1,3 +1,6 @@
+import './expenseDate.css'; 
+
+
 const ExpenseDate = (props) => {
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
     const month = props.date.toLocaleString('en-US', { month: 'long' });
@@ -5,10 +8,10 @@ const ExpenseDate = (props) => {
 
     return (
         <>
-        <div>
-            <div>{day}</div>
-            <div>{month}</div>
-            <div>{year}</div>
+        <div className="expense-date"> 
+            <div className="expense-date__day">{day}</div>
+            <div className="expense-date__month">{month}</div>
+            <div className="expense-date__year">{year}</div>
         </div>
         </>
     );
--- expenses/src/components/expenseItem.jsx
+++ expenses/src/components/expenseItem.jsx
@@ -1,3 +1,4 @@
+import './expenseItem.css'; 
 import ExpenseDate from './expenseDate.jsx';
 import './expenseItem.jsx';
 
@@ -7,9 +8,9 @@ const ExpenseItem = (props) => {
         <>
         <div className= "expense-item">
             <ExpenseDate date={props.data.date} />
-            <div className= "expense-item_description">
+            <div className= "expense-item__description">
                 <h2>{props.data.title}</h2>
-                <div className="expense-item_price">{props.data.price}</div>
+                <div className="expense-item__price">{props.data.price}</div>
             </div>
         </div>
         
