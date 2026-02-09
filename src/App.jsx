7d24306b5467a93329210a19aa5a4c9095163277
Prait Kirilo <prait.kirilo@gmail.com>
2026-02-09 08:45:17 +0200

    18. LocalStorage  ja useEffect hooki kasutamine

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,21 @@ import "./App.css";
 import ExpenseItem from "./components/Expenses/expenseItem.jsx";
 import Expenses from "./components/Expenses/expenses.jsx";
 import NewExpense from "./components/NewExpense/NewExpense.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const App = (props) => {
   const expenses = [
-    {
-      id: "1d",
-      date: new Date(2023, 10, 12),
-      title: "New book",
-      price: 30.99,
-    },
-    {
-      id: "2d",
-      date: new Date(2024, 10, 12),
-      title: "New jeans",
-      price: 99.99,
-    },
-    {
-      id: "3d",
-      date: new Date(2024, 10, 12),
-      title: "New jeans",
-      price: 139.99,
-    },
   ];
 
-  const [expense, setExpenses] = useState(expenses);
+const App = (props) => {
+  const [expense, setExpenses] = useState(() => {
+  const expenseFormLS = JSON.parse(localStorage.getItem("expenses"));
+  return expenseFormLS || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expense));
+  }, [expense]);
+
 
   const addExpenseHandler = (expense) => {
     setExpenses((previousExpense) => {
--- src/components/Expenses/expenses.jsx
+++ src/components/Expenses/expenses.jsx
@@ -14,7 +14,7 @@ const Expenses = (props) => {
   };
 
   const filteredExpenses = props.expenses.filter((expense) => {
-    return expense.date.getFullYear().toString() === filteredYear;
+    return new Date(expense.date).getFullYear().toString() === filteredYear;
   });
 
   let expenseContent = <p>No expenses found.</p>;
